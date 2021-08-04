8a32ea3324827fcba723946db2f927aa0fbd3430
suarez07 <76135635+suarez07@users.noreply.github.com>
2021-08-04 14:56:38 -0500

    Add files via upload

---
+++ Proyecto1.py
@@ -0,0 +1,4 @@
+print("Hola Mundo")
+print("Integrante número 1 - Harrison Smith Suarez Neuta")
+print("Integrante número 2 - Harold Arturo Rojas Camacho")
+print("Integrante número 3 - Brahian Sanchez Cardona")
